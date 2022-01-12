9e95fc7ef9915e2698a7295148e161ece05d86c2
Camilo <camiloagr2707@gmail.com>
2022-01-11 21:56:09 -0500

    initial animation for landing page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import Head from 'next/head';
+import Animation from '../components/Animation';
 
 export default function Home() {
   return (
-    <div>
+    <div id='controls' className='h-screen bg-zinc-800'>
       <Head>
         <title>Camilo Guevara</title>
       </Head>
       <h1 className='text-3xl'>The start of the portfolio</h1>
+      <Animation />
     </div>
   );
 }
